9b6cb51d4be581ee7f2ddd74149e809a8d437765
Dale-Biggs <biggs_dale@yahoo.com>
2017-11-30 23:33:41 -0500

    Menu Items UI update 1.1- Add Beverages and Home screen

--- BoardwalkApplication/app/src/main/java/com/example/klasik/boardwalkapplication/Beverages.java
+++ BoardwalkApplication/app/src/main/java/com/example/klasik/boardwalkapplication/Beverages.java
@@ -1,7 +1,9 @@
 package com.example.klasik.boardwalkapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Beverages extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class Beverages extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beverages);
+    }
+
+    public void milkshake(View view){
+        Intent intent=new Intent(this, Milkshake.class);
+        startActivity(intent);
+    }
+    public void coffee(View view){
+        Intent coffee=new Intent(this, Coffee.class);
+        startActivity(coffee);
+    }
+    public void smoothie(View view){
+        Intent smoothie=new Intent(this, Smoothie.class);
+        startActivity(smoothie);
     }
 }
